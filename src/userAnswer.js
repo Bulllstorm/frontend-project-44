65b67ddea501e06b493f95696041660497dc9424
Nikolay <kolia4246868@gmail.com>
2024-06-09 19:14:50 +0300

    fix userAnswer.js

--- src/userAnswer.js
+++ src/userAnswer.js
@@ -2,11 +2,14 @@ import readlineSync from "readline-sync";
 
 let userAnswer;
 function inputUserAnswer(){
-    userAnswer = readlineSync.question()
-    if(userAnswer !== 'yes' || userAnswer !== 'no'){
+    userAnswer = readlineSync.question('Your answer: ');
+    if(userAnswer === "yes" || userAnswer === "no"){
+        return userAnswer;
+    }
+    if(userAnswer !== "yes" || userAnswer !== "no"){
         userAnswer = Number(userAnswer);
         return userAnswer;
     }
-    return userAnswer;
+
 }
 export {userAnswer, inputUserAnswer};
