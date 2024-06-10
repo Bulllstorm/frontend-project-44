72bde8247ea581de1003e91adf8bddc9c04aa6b9
Nikolay <kolia4246868@gmail.com>
2024-06-10 18:14:35 +0300

    add brain-prime game

---
+++ bin/brain-prime.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+import {askName} from "../src/askName.js";
+import {inputUserAnswer} from "../src/userAnswer.js";
+import {compareAnswers} from "../src/compareAnswers.js";
+import {getCount} from "../src/getCount.js";
+
+let rightAnswer = '';
+let number = 0;
+let start = goBrainPrime;
+
+askName();
+goBrainPrime();
+
+function goBrainPrime() {
+    console.log('Answer "yes" if given number is prime. Otherwise answer "no"');
+    askQuestion();
+    inputUserAnswer();
+    isPrimeNumber(number)
+    compareAnswers(rightAnswer);
+    getCount(start, rightAnswer);
+}
+function isPrimeNumber(num){
+    for (let i = 2; i < num; i++){
+        if (num % i === 0){
+            rightAnswer = "no";
+            return rightAnswer;
+        }
+    }
+    rightAnswer = "yes";
+    return rightAnswer;
+}
+function askQuestion(){
+    console.log(`Question`, getNumber());
+}
+function getNumber(){
+    number =  Math.floor(Math.random()*(3571 - 2) + 2);
+    return number;
+}
